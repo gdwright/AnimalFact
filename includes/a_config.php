e4fd5104f19d746da6103e3bb26e6d243db1b6df
George <george@vounder.co.uk>
2019-01-20 21:47:38 +0000

    v 0.1 alpha
    MVP for random animal fact

---
+++ includes/a_config.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: georgewright
+ * Date: 2019-01-19
+ * Time: 21:21
+ */
+
+    $bgr = array(
+        'dan-wayman-1298942-unsplash.jpg',
+        'kamil-szumotalski-1298902-unsplash.jpg',
+        'cmdr-shane-1292615-unsplash.jpg',
+        'jorge-zapata-1297630-unsplash.jpg',
+        'francesco-de-tommaso-1287196-unsplash.jpg',
+        'henrique-jacob-1287736-unsplash.jpg',
+        'richard-lee-1289655-unsplash.jpg',
+        'thijs-schouten-1286607-unsplash.jpg',
+        'carrie-yang-1284872-unsplash.jpg',
+        'samia-liamani-1282599-unsplash.jpg',
+        'james-carol-lee-1279491-unsplash.jpg',
+        'balaji-malliswamy-1281469-unsplash.jpg',
+        'marcus-bengtsson-1279270-unsplash.jpg',
+        'gianna-scire-1280739-unsplash.jpg',
+        'ian-masters-1277374-unsplash.jpg',
+        'alexandra-marcu-1273567-unsplash.jpg',
+        'annie-spratt-1278319-unsplash.jpg',
+        'yannick-menard-1272924-unsplash.jpg',
+        'paola-galimberti-1272013-unsplash.jpg',
+        'ed-van-duijn-1271885-unsplash.jpg',
+        'michael-anfang-1270819-unsplash.jpg',
+        'thomas-bonometti-1252755-unsplash.jpg',
+        'thomas-bonometti-1252750-unsplash.jpg',
+        'matt-eason-625132-unsplash.jpg',
+        'matt-eason-625109-unsplash.jpg',
+        'isabel-galvez-1303622-unsplash.jpg'
+    ); // array of filenames
+
+    $fct = array(
+        array('Cats sleep 16-18 hours per day', 'A group of kittens is called a kindle, and a group of cats is called a clowder.'),
+        array('There are approximately 60 different species of Eagle', 'Eagles are monogamous, so generally mate for life'),
+        array('Horses cannot vomit.', 'On the underside of a horse’s hoof is a triangular shaped area called the “frog," which acts as a shock absorber for a horse’s leg, and also helps to pump blood back up the leg.'),
+        array('Turkeys are named after the country.', 'Female turkeys do not gobble.'),
+        array('In Malay orang means “person” and utan is derived from hutan, which means “forest.” Thus, orangutan literally means “person of the forest.”', 'Orangutans arms stretch out longer than their bodies – up to 8 ft. from fingertip to fingertip in the case of very large males.'),
+        array('Macaws are long-lived, living for as long as 60 years in the wild.', 'There are roughly 20 species of macaws, many of which are endangered and critically endangered.'),
+        array('There are approximately 60 different species of Eagle', 'Eagles are monogamous, so generally mate for life'),
+        array('Some breeds of falcon can see ultraviolet light which allows them to see trails of urine left by their prey, ultimately leading to dinner.', 'Falcon eyes are fifteen times proportionally larger than a humans.'),
+        array('Dogs only have sweat glands in their paws.', 'The earliest European images of dogs date back 12,000 years ago to cave drawings found in Spain.'),
+        array('Dogs only have sweat glands in their paws.', 'The earliest European images of dogs date back 12,000 years ago to cave drawings found in Spain.'),
+        array('Rhinoceros comes from greek. ‘Rhino’ meaning nose and ‘ceros’ meaning horn combine to create the word rhinoceros.', 'A group of Rhinos is called a "crash".'),
+        array('Tahrs are a large Asian species related to the wild goat.', 'Tahrs are an asian mammal.  However, there is a population of Tahrs on Table Mountain in South Africa.  These descended from a pair of tahrs which escaped from a zoo in 1936'),
+        array('British blue tits are strictly resident, seldom moving more than 15 miles from where they hatched.', 'The blue tit breeding season varies with location, but generally starts in the third week of April.'),
+        array('Monkeys can understand written numbers and can even count.', 'Scientists have observed female monkeys teaching their young how to floss their teeth.'),
+        array('Horses cannot vomit.', 'On the underside of a horse’s hoof is a triangular shaped area called the “frog," which acts as a shock absorber for a horse’s leg, and also helps to pump blood back up the leg.'),
+        array('The average bee will make only 1/12th of a teaspoon of honey in its lifetime.', 'Honey bees communicate with one another by dancing.'),
+        array('A polar bear’s fur isn’t white. It’s actually transparent with a hollow core that reflects light.', 'Polar bears are classified as marine mammals.'),
+        array('Killer whales aren’t whales, they’re actually dolphins.', 'Dolphins have few natural enemies. Humans are their main threat. Pollution, fishing and hunting mean some dolphin species have an uncertain future.'),
+        array('Macaws are long-lived, living for as long as 60 years in the wild.', 'There are roughly 20 species of macaws, many of which are endangered and critically endangered.'),
+        array('Every year that a frog goes into hibernation, a new layer of bone forms.', 'The study of amphibians and reptiles is called Herpetology, and those who study them are called Herpetologists.'),
+        array('The chicken, Gallus gallus domesticus, is a domestic subspecies of the red junglefowl, a member of the pheasant family that is native to Asia.', 'Chickens were first domesticated for the purpose of cockfights, not as food.'),
+        array('In fall a brown bear may eat as much as 90 pounds of food each day, and it may weigh twice as much before hibernation as it will in spring.', 'Adult brown bears are powerful, top-of-the-food chain predators, but much of their diet consists of nuts, berries, fruit, leaves, and roots.'),
+        array('When wolves are successful in hunting their prey they do not eat in moderation. A single animal can consume 20 pounds of meat at a sitting.', 'Wolfpacks are established according to a strict hierarchy, with a dominant male at the top and his mate not far behind. Usually this male and female are the only animals of the pack to breed.'),
+        array('A group of meerkats is called a "mob", "gang" or "clan".', 'The word "meerkat" originates from Afrikaans and translates to "lake cat".  However, meerkats are not cats at all and belong to the mongoose family.'),
+        array('The name Lemur is actually Latin for “spirits of the night”.', 'The lemur is a native animal of Madagascar.'),
+        array('There are around 300 species of octopus and they are found in every ocean.', 'Octopuses form opinions about individual people. One routinely squirted water down the back of a keeper it seemed to dislike')
+    ); // array of fact arrays
+
+    $i = rand(0, count($bgr)-1); // generate random number size of the array
+
+    $k = rand(0, count($fct[$i])-1); // generate placeholder rand for fact
+    $selectedBgr = "$bgr[$i]"; // set variable equal to which random filename was chosen
+    $selectedFct = $fct[$i][$k]; //$fct[$i][$k]
+?>
