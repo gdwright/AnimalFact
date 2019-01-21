fe5783b6280c88d36251414fab67e3bfc2669745
George <george@vounder.co.uk>
2019-01-21 13:14:36 +0000

    first alpha version

---
+++ htdocs/index.php
@@ -0,0 +1,33 @@
+
+<?php include("includes/a_config.php");?>
+
+
+
+<style type="text/css">
+    <!--
+    body{
+        background: url(images/<?php echo $selectedBgr; ?>) no-repeat;
+        background-size: cover;
+        background-position: center center;
+    }
+    -->
+</style>
+
+<body>
+<div class = "container">
+    <p style = "
+        background-color: rgba(256, 256, 256, 0.7);
+        font-family:  tahoma;
+        margin-left:  auto;
+        margin-right: auto;
+        width: 36em;
+        position: center;
+    ">
+        <?php echo $selectedFct; ?>
+    </p>
+</div>
+</body>
+
+
+
+
