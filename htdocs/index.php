88d085c2c5a57e27d43b43d3ea7b1c8d3d40963a
George <george@vounder.co.uk>
2020-12-17 13:08:56 +0000

    put random variables back into index

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,6 +3,15 @@
 
 
 
+<?php
+
+    $i = rand(0, count($bgr)-1); // generate random number size of the array
+
+    $k = rand(0, count($fct[$i])-1); // generate placeholder rand for fact
+    $selectedBgr = "$bgr[$i]"; // set variable equal to which random filename was chosen
+    $selectedFct = $fct[$i][$k]; //$fct[$i][$k]
+?>
+
 <style type="text/css">
     <!--
     body{
@@ -14,6 +23,8 @@
 </style>
 
 <body>
+
+
 <div class = "container">
     <p style = "
         background-color: rgba(256, 256, 256, 0.7);
